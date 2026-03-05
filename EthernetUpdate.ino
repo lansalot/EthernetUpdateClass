be416d41f31dcc3d4852f99b5a8b8ef4215ddd39
lansalot <lansalot@gmail.com>
2026-03-05 22:38:41 +0000

    go

--- EthernetUpdate.ino
+++
@@ -1,2 +0,0 @@
-// Legacy firmware update logic moved into EthernetUpdater class.
-// Kept as a placeholder to avoid build/project file churn.
---
+++ EthernetUpdateClass.ino
@@ -0,0 +1,42 @@
+#include "PGNs.h"
+#include <NativeEthernet.h>
+#include <NativeEthernetUdp.h>
+
+struct ConfigIP
+{
+    uint8_t ipOne = 192;
+    uint8_t ipTwo = 168;
+    uint8_t ipThree = 1;
+};
+ConfigIP networkAddress; // 3 bytes
+
+// IP & MAC address of this module of this module
+byte Eth_myip[4] = {0, 0, 0, 0}; // This is now set via AgIO
+byte mac[] = {0x00, 0x00, 0x56, 0x00, 0x00, 0x78};
+
+IPAddress Eth_ipDestination;
+
+#define ModuleID 0
+#define InoType 0
+
+#include "EthernetUpdater.h"
+
+EthernetUpdater updater(ModuleID, InoType);
+
+
+// Setup procedure ------------------------
+void setup()
+{
+    delay(1000);
+    Serial.begin(115200);
+    delay(100);
+    Serial.println("Start setup");
+    EthernetStart();
+    updater.begin();
+    Serial.println("\r\nEnd setup, loop time!");
+}
+
+void loop()
+{
+    updater.poll();
+}
